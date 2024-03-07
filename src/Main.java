f37f4035bedea9c2eb8cfbb5dbbab8969e50675c
GrayPheonix <vrm3gaming@gmail.com>
2024-03-07 17:27:35 -0500

    starting jframe

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.*;
+import java.awt.Frame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame j = new JFrame();
+
+        j.setSize(500, 300);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.isVisible();
+        j.isEnabled();
+    }
+
+}
