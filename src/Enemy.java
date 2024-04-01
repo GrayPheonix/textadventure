3f20ff603250ee1436b2960e3b6eac23df56b7ae
Hamham6 <157623847+Hamham6@users.noreply.github.com>
2024-04-01 12:13:01 -0400

    Update Enemy.java

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,6 +2,10 @@
 
 public class Enemy{
 
+    //Enemy Variables
+    private String[] enemies = {"Wolf", "Assassin", "Golem", "Dragon"};
+    private int maxEnemyHealth = 50;
+    private int maxAttackDamage = 25;
 
 
 }
