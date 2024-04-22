c49fb3cb2aaf0639f71016a2b2e2723ae0e2f0cf
GrayPheonix <vrm3gaming@gmail.com>
2024-04-21 20:19:54 -0400

    Smoothing some things out
    Adding typewritter and buttons

--- src/GAME.java
+++ src/GAME.java
@@ -1,125 +1,323 @@
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.*;
 
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class GAME {
-    Scanner in = new Scanner(System.in);
+public class GAME implements ActionListener {
     Random rand = new Random();
 
-    public void Start() {
-        boolean running = true; //is this used?
+    ArrayList<Object> arrayList = new ArrayList<>();
+    //Enemy Variables
+    String[] enemies = {"Wolf", "Assassin", "Golem", "Dragon"};
+    int maxEnemyHealth = 50;
+    int maxAttackDamage = 25;
 
-        //could let the person name the player?
-        Player player = Player.getPlayer(); //get the player instance
-        Inventory inventory = Inventory.getPlayerInventory(); //get the inventory instance
-        System.out.println("You have entered the Dungeon");
+    //Player Variables
+    int health = 100;
+    int attackDamage = 35;
+    int totalHealthPotions = 3;
+    int healthPotionHealing = 30;
+    int healthPotionDropchance = 40; //Drop percentage from enemies
+    int monstersDefeated = 0;
+    int ranAwayTimes = 0;
+    int potionsDrank = 0;
+    int goldDrop = 15;
+    int totalGold = 0;
+    int gold = 0;
+
+    Insets insets;
+
+    JLabel l = new JLabel();
+    JLabel l2 = new JLabel();
+    JLabel l3 = new JLabel();
+    JLabel l4 = new JLabel();
+    JLabel l5 = new JLabel();
+    JLabel l6 = new JLabel();
+    JLabel l7 = new JLabel();
+    JLabel l8 = new JLabel();
+    JLabel l9 = new JLabel();
+    JLabel l10 = new JLabel();
+    JButton a = new JButton("> 1. Attack");
+    JButton d = new JButton("> 2. Drink Potion");
+    JButton r = new JButton("> 3. Run Away");
+    JButton f = new JButton("> 1. Find another Monster");
+    JButton le = new JButton("> 2. Leave The dungeon!");
+    JButton s = new JButton("> 3. Upgrade your sword!");
+
+    public void GUI(){
+        JFrame j = new JFrame();
+        j.setTitle("Dungeon Run");
+        j.setLocationRelativeTo(null);
+        j.setResizable(false);
+        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        j.setLayout(null);
+        j.setSize(1000, 600);
+        j.setBackground(Color.BLACK);
+
+        JTextField t = new JTextField();
+        t.setSize(1000,600);
+        t.setBackground(Color.BLACK);
+        t.setForeground(Color.BLACK);
+        j.add(t);
+        t.setText(null);
+
+        Font font = new Font("Serif",0 , 50);
+
+        l.setBorder(null);
+        l.setBounds(10,10,980,50);
+        l.getPreferredSize();
+        l.setHorizontalAlignment(JLabel.RIGHT);
+        l.setFont(font);
+        l.setForeground(new Color(250, 231, 110));
+        l.isVisible();
+        l.setVisible(true);
+        l2.setBorder(null);
+        l2.setBounds(10,70,980,50);
+        l2.getPreferredSize();
+        l2.setHorizontalAlignment(JLabel.RIGHT);
+        l2.setFont(font);
+        l2.setForeground(new Color(250, 231, 110));
+        l2.isVisible();
+        l2.setVisible(true);
+        l3.setBorder(null);
+        l3.setBounds(10,130,980,50);
+        l3.getPreferredSize();
+        l3.setHorizontalAlignment(JLabel.RIGHT);
+        l3.setFont(font);
+        l3.setForeground(new Color(250, 231, 110));
+        l3.isVisible();
+        l3.setVisible(true);
+        l4.setBorder(null);
+        l4.setBounds(10,190,980,50);
+        l4.getPreferredSize();
+        l4.setHorizontalAlignment(JLabel.RIGHT);
+        l4.setFont(font);
+        l4.setForeground(new Color(250, 231, 110));
+        l4.isVisible();
+        l4.setVisible(true);
+        l5.setBorder(null);
+        l5.setBounds(10,250,980,50);
+        l5.getPreferredSize();
+        l5.setHorizontalAlignment(JLabel.RIGHT);
+        l5.setFont(font);
+        l5.setForeground(new Color(250, 231, 110));
+        l5.isVisible();
+        l5.setVisible(true);
+        l6.setBorder(null);
+        l6.setBounds(10,310,980,50);
+        l6.getPreferredSize();
+        l6.setHorizontalAlignment(JLabel.RIGHT);
+        l6.setFont(font);
+        l6.setForeground(new Color(250, 231, 110));
+        l6.isVisible();
+        l6.setVisible(true);
+        l7.setBorder(null);
+        l7.setBounds(10,370,980,50);
+        l7.getPreferredSize();
+        l7.setHorizontalAlignment(JLabel.RIGHT);
+        l7.setFont(font);
+        l7.setForeground(new Color(250, 231, 110));
+        l7.isVisible();
+        l7.setVisible(true);
+        l8.setBorder(null);
+        l8.setBounds(10,430,980,50);
+        l8.getPreferredSize();
+        l8.setHorizontalAlignment(JLabel.RIGHT);
+        l8.setFont(font);
+        l8.setForeground(new Color(250, 231, 110));
+        l8.isVisible();
+        l8.setVisible(true);
+        l9.setBorder(null);
+        l9.setBounds(10,490,980,50);
+        l9.getPreferredSize();
+        l9.setHorizontalAlignment(JLabel.RIGHT);
+        l9.setFont(font);
+        l9.setForeground(new Color(250, 231, 110));
+        l9.isVisible();
+        l9.setVisible(true);
+        l10.setBorder(null);
+        l10.setBounds(10,550,980,50);
+        l10.getPreferredSize();
+        l10.setHorizontalAlignment(JLabel.RIGHT);
+        l10.setFont(font);
+        l10.setForeground(new Color(250, 231, 110));
+        l10.isVisible();
+        l10.setVisible(true);
+
+        a.setBorder(null);
+        a.setBounds(10,250,980,50);
+        a.getPreferredSize();
+        a.setHorizontalAlignment(JLabel.RIGHT);
+        a.setFont(font);
+        a.setForeground(new Color(250, 231, 110));
+        d.setBorder(null);
+        d.setBounds(10,310,980,50);
+        d.getPreferredSize();
+        d.setHorizontalAlignment(JLabel.RIGHT);
+        d.setFont(font);
+        d.setForeground(new Color(250, 231, 110));
+        r.setBorder(null);
+        r.setBounds(10,370,980,50);
+        r.getPreferredSize();
+        r.setHorizontalAlignment(JLabel.RIGHT);
+        r.setFont(font);
+        r.setForeground(new Color(250, 231, 110));
+        f.setBorder(null);
+        f.setBounds(10,430,980,50);
+        f.getPreferredSize();
+        f.setHorizontalAlignment(JLabel.RIGHT);
+        f.setFont(font);
+        f.setForeground(new Color(250, 231, 110));
+        le.setBorder(null);
+        le.setBounds(10,490,980,50);
+        le.getPreferredSize();
+        le.setHorizontalAlignment(JLabel.RIGHT);
+        le.setFont(font);
+        le.setForeground(new Color(250, 231, 110));
+        s.setBorder(null);
+        s.setBounds(10,550,980,50);
+        s.getPreferredSize();
+        s.setHorizontalAlignment(JLabel.RIGHT);
+        s.setFont(font);
+        s.setForeground(new Color(250, 231, 110));
+
+        j.isVisible();
+        j.setVisible(true);
+        j.isEnabled();
+    }
+
+    public static void typewriterEffect(JLabel label, String str) {
+        String temp = "";
+        for(int i = 0; i<str.length(); i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ie) {
+                Thread.currentThread().interrupt();
+            }
+            temp = str.substring(0,i);
+            label.setText(temp);
+        }
+        label.setText(str);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        e.getSource();
+        arrayList.add(e);
+        l.setText("");
+        l2.setText("");
+        l3.setText("");
+        l4.setText("");
+        l5.setText("");
+        l6.setText("");
+        l7.setText("");
+        l8.setText("");
+        l9.setText("");
+    }
+
+    public void Start() {
+        boolean running = true;
+        GUI();
+        typewriterEffect(l, "> You have entered the Dungeon");
 
         GAME:
         while (true) {
-            System.out.println("****************************************");
+            typewriterEffect(l2,"****************************************");
+            l.setText("");
+            l2.setText("");
+            int enemyHealth = rand.nextInt(maxEnemyHealth);
+            String enemy = enemies[rand.nextInt(enemies.length)];
+            typewriterEffect(l,"> * " + enemy + " has appeared! *");
 
-            //right now the only thing you can do is fight so an enemy gets
-            //generated right off the bat. If other stuff becomes possible then
-            //the combat event would become another class, I think.
-            Enemy enemy = new Enemy();
-            enemy.initiate();
-
-            System.out.println("\t* " + enemy.getEnemyName() + " has appeared! *\n");
-
-            //combat options
             label:
-            while (enemy.getEnemyHealth() > 0) {
-                System.out.println("\t Your Health: " + player.getHealth());
-                System.out.println("\t" + enemy.getEnemyName() + "'s Health: " + enemy.getEnemyHealth());
-                System.out.println("\n\tWhat will you do?");
-                System.out.println("\t1. Attack");
-                System.out.println("\t2. Drink Potion");
-                System.out.println("\t3. Run Away");
-                String input = in.nextLine();
-                switch (input) {
-                    case "1" -> { //attack
-                        int damageDealt = rand.nextInt(player.getAttackDamage());
-                        int damageTaken = rand.nextInt(player.getMaxAttackDamage());
+            while (enemyHealth > 0) {
+                typewriterEffect(l2,"> Your Health: " + health);
+                typewriterEffect(l3,"> " + enemy + "'s Health: " + enemyHealth);
+                typewriterEffect(l4,"> What will you do?");
+                a.setVisible(true);
+                typewriterEffect(l5,a.getText());
+                d.setVisible(true);
+                typewriterEffect(l6,d.getText());
+                r.setVisible(true);
+                typewriterEffect(l7,r.getText());
 
-                        enemy.setEnemyHealth(enemy.getEnemyHealth()-damageDealt);
-                        player.setHealth(player.getHealth()-damageTaken);
 
-                        System.out.println("\t> You hit the " + enemy.getEnemyName() + " for " + damageDealt + " damage!");
-                        System.out.println("\t> You took " + damageTaken + " points of damage!");
-
-                        if (player.getHealth() < 1) {
-                            System.out.println("\t> You are dead!");
-                            break label;
-                        }
+                if (arrayList.contains(a)) {
+                    int damageDealt = rand.nextInt(attackDamage);
+                    int damageTaken = rand.nextInt(maxAttackDamage);
+                    enemyHealth -= damageDealt;
+                    health -= damageTaken;
+                    typewriterEffect(l,"> You hit the " + enemy + " for " + damageDealt + " damage!");
+                    typewriterEffect(l2,"> You took " + damageTaken + " points of damage!");
+                    if (health < 1) {
+                        typewriterEffect(l3,"> You are dead!");
+                        break;
                     }
-                    case "2" -> { //use potion
-                        HealingPotion healingPotion = (HealingPotion) inventory.getItem("healingPotion");
-                        //if there is a HealingPotion instance then healingPotion is now equal to that instance,
-                        //if there is not then healingPotion is now equal to null
-                        if (healingPotion != null) {
-                            int healthPotionHealing = healingPotion.getHealthPotionHealing();
-                            healingPotion.use();
-                            System.out.println("\t You drank a Health Potion! " +
-                                    "You healed for " + healthPotionHealing + " points of health!" +
-                                    "\n\t> You now have " + player.getHealth() + " HP! " +
-                                    "\n\t You now have: " + healingPotion.getTotal() + " health potions!");
-                        } else {
-                            System.out.println("\t You have no health potions!");
-                        }
+                } else if (arrayList.contains(d)) {
+                    if (totalHealthPotions > 0) {
+                        health += healthPotionHealing;
+                        --totalHealthPotions;
+                        ++potionsDrank;
+                        typewriterEffect(l,"> You drank a Health Potion! You healed for " + healthPotionHealing + " points of health!");
+                        typewriterEffect(l2,"> You now have " + health + " HP!");
+                        typewriterEffect(l3,"> You now have: " + totalHealthPotions + " health potions!");
+                    } else {
+                        typewriterEffect(l,"> You have no health potions!");
                     }
-                    case "3" -> {
-                        System.out.println("\t You successfully ran away from the " + enemy + "!");
-                        player.addRanAwayTimes(); //add to lifetime stat
+                } else {
+                    if (arrayList.contains(r)) {
+                        typewriterEffect(l,"> You successfully ran away from the " + enemy + "!");
+                        ++ranAwayTimes;
                         continue GAME;
                     }
-                    default -> {
-                        System.out.println("\t Nothing Happened!");
-                        if (player.getHealth() < 1) {
-                            System.out.println("\t You died!");
-                        }
-                    }
+                    typewriterEffect(l2,"> Nothing happened!");
                 }
             }
 
-            player.addMonstersDefeated(); //add to lifetime stat
-            System.out.println("****************************************");
-            System.out.println(" * " + enemy.getEnemyName() + " was destroyed! *");
-            System.out.println(" * You have " + player.getHealth() + " health remaining. * ");
-            if (rand.nextInt(100) < enemy.getHealthPotionDropchance()) {
-                HealingPotion healingPotion = new HealingPotion();
-                inventory.add(healingPotion);
-                healingPotion.changeTotalBy(1);
-                System.out.println(" * The " + enemy + " dropped a health potion! * ");
-                System.out.println(" * You now have " + healingPotion.getTotal() + " health potions! * ");
+            monstersDefeated++;
+            ++totalGold;
+            typewriterEffect(l,"****************************************");
+            typewriterEffect(l2,"> * " + enemy + " was destroyed! *");
+            typewriterEffect(l3,"> * You have " + health + " health remaining. * ");
+            typewriterEffect(l4," * You now have " + totalGold + " gold! * ");
+            if (rand.nextInt(100) < healthPotionDropchance) {
+                totalHealthPotions++;
+                typewriterEffect(l4,"> * The " + enemy + " dropped a health potion! * ");
+                typewriterEffect(l5,"> * You now have " + totalHealthPotions + " health potions! * ");
             }
-            System.out.println("****************************************");
-            System.out.println("What would you like to do?");
-            System.out.println("1. Find another Monster");
-            System.out.println("2. Leave the dungeon!");
+            typewriterEffect(l6,"****************************************");
+            typewriterEffect(l7,"> What would you like to do?");
+            f.setVisible(true);
+            typewriterEffect(l8,f.getText());
+            le.setVisible(true);
+            typewriterEffect(l9,le.getText());
+            s.setVisible(true);
+            typewriterEffect(l10,s.getText());
 
-            String input = in.nextLine();
 
-            while (!input.equals("1") && !input.equals("2")) {
-                System.out.println("Nothing Happened!");
-                input = in.nextLine();
-            }
-
-            if (input.equals("1")) {
-                System.out.println("You search for more monsters.");
+            if (arrayList.contains(f)) {
+                typewriterEffect(l,"> You search for more monsters.");
             } else {
-                System.out.println("You exited the dungeon!");
+                typewriterEffect(l,"> You exited the dungeon!");
             }
+            l.setText("");
+            l2.setText("");
+            l3.setText("");
+            l4.setText("");
+            l5.setText("");
+            l6.setText("");
+            l7.setText("");
+            l8.setText("");
+            l9.setText("");
 
-            System.out.println("***************************");
-            System.out.println(" * Thanks For Playing! * ");
-            System.out.println(" * You killed: " + player.getMonstersDefeated() + " monsters!");
-            System.out.println(" * You ran away: " + player.getRanAwayTimes() + " times!");
-            System.out.println(" * You drank " + player.getTotalPotionsDrank() + " potions!");
-            System.out.println("***************************");
+            typewriterEffect(l,"***************************");
+            typewriterEffect(l2,"> * Thanks For Playing! * ");
+            typewriterEffect(l3,"> * You killed: " + monstersDefeated + " monsters!");
+            typewriterEffect(l4,"> * You ran away: " + ranAwayTimes + " times!");
+            typewriterEffect(l5,"> * You drank " + potionsDrank + " potions!");
+            typewriterEffect(l6,"***************************");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,37 +6,17 @@ import javax.swing.JTextField;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 
 
 public class Main {
     public static void main(String[] args) {
-        JFrame j = new JFrame();
-        JButton b = new JButton();
         GAME n = new GAME();
-        j.setTitle("Dungeon Run");
-        j.setLocationRelativeTo(null);
-        j.setResizable(false);
-        j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        j.setLayout(null);
-        j.setSize(1000, 600);
-        j.setBackground(Color.BLACK);
+        n.Start();
 
-        JTextField textPart = new JTextField();
-        textPart.setBounds(8,10,972,550);
-        textPart.setBackground(Color.BLACK);
-        j.add(textPart);
-        textPart.setText(null);
-        Font font = new Font("Serif",0 , 50);
-        textPart.setFont(font);
-        textPart.setHorizontalAlignment(JTextField.RIGHT);
-        textPart.setSelectedTextColor(Color.LIGHT_GRAY );
-        textPart.setForeground(Color.WHITE );
-
-        j.isVisible();
-        j.setVisible(true);
-        j.isEnabled();
     }
-
 }
+
